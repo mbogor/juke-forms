c8c649b0a99ee91d7602909ae4d6336b337252cc
Jisoo Shin <jisooxshin@gmail.com>
2016-02-15 14:51:47 -0500

    some validations working

--- browser/js/addPlaylist/playlist.controller.js
+++ browser/js/addPlaylist/playlist.controller.js
@@ -3,22 +3,10 @@
 /* PLAYLIST (SINGULAR) CONTROLLER */
 
 juke.controller('NewPlaylistCtrl', function ($scope) {
-	$scope.touchedTitle = false;
 
     $scope.create = function(newPlaylist){
     	$scope.newForm = newPlaylist;
     	console.log($scope.newForm)
     }
-
-    $scope.TitleError = function(){
-    	if ($scope.touchedTitle){
-    		return true;
-    	}
-    }
-
-    $scope.SongError = function(){
-
-    }
-
 
 });
